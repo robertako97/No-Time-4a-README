67375ef097f81fe82927906a093e462f97d3656c
robertako97 <roberto.diazgmx@gmail.com>
2023-08-06 19:17:46 -0700

    Added NPMs

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+//Inquirer Added
+//Jquery Added (Not needed?)
+const { JSDOM } = require( "jsdom" ); //Call a virtual DOM env
+const { window } = new JSDOM( "" ); //Simulate HTML
+const $ = require( "jquery" )( window ); //Use Jquery in virtual window
 
 // TODO: Create an array of questions for user input
 const questions = [];
