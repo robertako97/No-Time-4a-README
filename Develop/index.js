bd767956222e025c7698e3b1ad42d30864cc4ba1
robertako97 <roberto.diazgmx@gmail.com>
2023-08-07 22:19:52 -0700

    Added license markdown if statements for testing, compiled some answers on console - next commitment to create a draft file with no markdowns

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,113 @@
 // TODO: Include packages needed for this application
 //Inquirer Added
 //Jquery Added (Not needed?)
-const { JSDOM } = require( "jsdom" ); //Call a virtual DOM env
-const { window } = new JSDOM( "" ); //Simulate HTML
-const $ = require( "jquery" )( window ); //Use Jquery in virtual window
+//const { JSDOM } = require( "jsdom" ); //Call a virtual DOM env
+//const { window } = new JSDOM( "" ); //Simulate HTML
+//const $ = require( "jquery" )( window ); //Use Jquery in virtual window
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+//Constant README file template name
+
+// Markdowns
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+/*
+const questions = {
+    title: [],
+    description: [],
+    installation: [],
+    usage: [],
+    contributing: [],
+    instructions: [],
+    license: [],
+    username: [],
+    email: []
+
+};
+*/
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+
+ //   const content = typeof data === 'string' ? data : JSON.stringify(data, null, 2);
+    // Write the data to the specified file
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log(`Data has been written to ${fileName}`);
+        }
+    });
+
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your project title?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Project description:',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'Installation instructions:',
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Usage information:',
+                name: 'usage',
+            },
+            {
+                type: 'list',
+                message: "Choose a license for your project.",
+                choices: ['MIT', 'Mozilla', 'Unlicense'],
+                name: 'license'
+            }
+
+
+
+
+        ])
+        .then((answers) => {
+            console.log(answers);
+
+
+            // Create/Write README
+         //   writeToFile(fileName, answers);
+
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.error("Prompt couldn't be rendered in the current environment");
+            } else {
+                console.error('Something else went wrong');
+            }
+        });
+
+
+
+
+
+
+
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,67 @@
+let badge = "";
+let badgeLink ="";
+let licenseSection = "";
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string -> Added unlicensed badge
+function renderLicenseBadge(license) {
+
+  if (license === "MIT") {
+    badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n" +
+    "\n" ;
+  }
+
+  else if (license === "Mozilla") {
+    badge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n" +
+        "\n" ;
+  }
+
+  else if (license === "Unlicense") {
+badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n" +
+    "\n" ; }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  if (license === "MIT") {
+    badgeLink = "(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n" +
+        "\n" ;
+  }
+
+  else if (license === "Mozilla") {
+    badgeLink = "(https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n" +
+        "\n" ;
+  }
+
+  else if (license === "Unlicense") {
+    badgeLink = "(https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n" +
+        "\n" ; }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  if (license === "MIT") {
+    licenseSection = "TBD License Section MIT";
+  }
+
+  else if (license === "Mozilla") {
+    licenseSection = "TBD License Section Mozilla";
+
+  }
+
+  else if (license === "Unlicense") {
+    licenseSection = "TBD License Section Unlicense";
+
+  }
+
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
